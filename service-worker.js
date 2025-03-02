65ef5e3397b7e9dfc442e88752362905c8d6bdab
Sercan Külcü <sercankulcu@gmail.com>
2025-03-02 23:03:15 +0300

    .

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'dorukdayim-cache-v4';
+const urlsToCache = [
+    'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js',
+    'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js',
+    'https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap'
+];
+
+// Install the service worker and cache assets
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Serve cached content when offline
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                //return response || fetch(event.request);
+                return fetch(event.request);
+            })
+    );
+});
+
+// Update the service worker and remove old caches
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
